b0fcec9154b7e7d8154f0ffd26111de5cf793cc1
Matthew Lock <matthew_lock20@hotmail.com>
2020-06-02 11:46:02 +0100

    Adding failing test

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,8 @@
 def fizzbuzz(number)
   "fizz"
+  if number == 5
+    "buzz"
+  else number == 3
+    "fizz"
+end
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -7,4 +7,7 @@ describe 'fizzbuzz' do
   it 'returns "buzz" when passed 5' do
     expect(fizzbuzz(5)).to eq 'buzz'
   end
+  it 'returns (number) when number is not a multiple of 3' do 
+    expect(fizzbuzz(4)).to eq 4
+  end
 end
